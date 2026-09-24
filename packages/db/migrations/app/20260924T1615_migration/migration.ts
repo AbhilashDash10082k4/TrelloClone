b93075ca3afec360f062251b9b6da39858cbaa06
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2026-09-24 21:54:27 +0530

    migration graph fixed

--- packages/db/migrations/app/20260924T1615_migration/migration.ts
+++ packages/db/migrations/app/20260924T1615_migration/migration.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S node
-import type { Contract as Start } from '../../snapshots/0abbafb354b90e5e9b29c590f3778ef25b78db4ef86080318609eb901022eeaa/contract';
-import startContract from '../../snapshots/0abbafb354b90e5e9b29c590f3778ef25b78db4ef86080318609eb901022eeaa/contract.json' with { type: 'json' };
+import type { Contract as Start } from '../../snapshots/169e1a7bbc0f59bb701403139f8b3385bef32a85aa729d6219af83fa45f7c3dd/contract';
+import startContract from '../../snapshots/169e1a7bbc0f59bb701403139f8b3385bef32a85aa729d6219af83fa45f7c3dd/contract.json' with { type: 'json' };
 import type { Contract as End } from '../../snapshots/7f22a2fcf8dc3a88ac4961a30b043055b12c550f198f081bc40f373f9ddbf11c/contract';
 import endContract from '../../snapshots/7f22a2fcf8dc3a88ac4961a30b043055b12c550f198f081bc40f373f9ddbf11c/contract.json' with { type: 'json' };
 import { Migration, MigrationCLI, col, placeholder } from '@prisma/orm-postgres/migration';
@@ -14,24 +14,9 @@ export default class M extends Migration<Start, End> {
       this.dropDefault({ schema: 'public', table: 'board', column: 'updatedAt' }),
       this.dropDefault({ schema: 'public', table: 'comment', column: 'updatedAt' }),
       this.dropDefault({ schema: 'public', table: 'org', column: 'updatedAt' }),
-      this.dropDefault({ schema: 'public', table: 'project', column: 'updatedAt' }),
       this.dropDefault({ schema: 'public', table: 'section', column: 'updatedAt' }),
       this.dropDefault({ schema: 'public', table: 'task', column: 'updatedAt' }),
       this.dropDefault({ schema: 'public', table: 'user', column: 'updatedAt' }),
-      this.addColumn({
-        schema: 'public',
-        table: 'project',
-        column: col('endDate', 'timestamptz', {
-          codecRef: { codecId: 'pg/timestamptz-temporal@1' },
-        }),
-      }),
-      this.addColumn({
-        schema: 'public',
-        table: 'project',
-        column: col('startDate', 'timestamptz', {
-          codecRef: { codecId: 'pg/timestamptz-temporal@1' },
-        }),
-      }),
       this.addColumn({
         schema: 'public',
         table: 'taskMapping',
